9175ccf7b28840f8b09b36471381d7d6c246d127
Aleksandr Klevko <aleksandr_klevko@epam.com>
2018-04-13 14:24:32 +0300

    demo docs commit

--- docs/script.js
+++ docs/script.js
@@ -8,86 +8,232 @@
       .controller('InlineEditingController', InlineEditingController);
 
   function controller($scope) {
-    $scope.stickyRows = [
-      {
-        value: 'Potassium hydroxide'
-      },
-      {
-        value: 'Hydrochloric acid'
-      }
-    ];
-    $scope.bodyRows = [
-      {
-        formula: {
-          value: 'KOH'
+    $scope.reactantRows = {
+      sticky: [
+        {
+          value: 'Potassium hydroxide'
         },
-        mw: {
-          value: 56.106
-        },
-        equiv: {
-          value: 1
-        },
-        mass: {
-          value: 5.00,
-          units: ['mg', 'g', 'kg']
-        },
-        volume: {
-          value: 6
-        },
-        purity: {
-          value: 100
-        },
-        quantifier: {
-          value: 0.25,
-          units: ['g/mL', 'g/L', 'kg/L']
-        },
-        n: {
-          value: 19.213,
-          units: ['mol', 'mmol']
-        },
-        comment: {
-          value: ''
+        {
+          value: 'Hydrochloric acid'
         }
-      },
-      {
-        formula: {
-          value: 'HCl'
-        },
-        mw: {
-          value: 36.461
-        },
-        equiv: {
-          value: 1
-        },
-        mass: {
-          value: 3.249,
-          units: ['mg', 'g', 'kg']
-        },
-        volume: {
-          value: 4
-        },
-        purity: {
-          value: 100
-        },
-        quantifier: {
-          value: 0.25,
-          units: ['g/mL', 'g/L', 'kg/L']
-        },
-        n: {
-          value: 19.213,
-          units: ['mol', 'mmol']
-        },
-        comment: {
-          value: 'This is a long-long-long comment'
-        }
-      }
-    ];
 
-    $scope.addNew = function () {
-      var newStickyRow = {
+      ],
+      body: [
+        {
+          formula: {
+            value: 'KOH'
+          },
+          mw: {
+            value: 56.106
+          },
+          equiv: {
+            value: 1
+          },
+          mass: {
+            value: 5.00,
+            units: ['mg', 'g', 'kg']
+          },
+          volume: {
+            value: 6
+          },
+          purity: {
+            value: 100
+          },
+          quantifier: {
+            value: 0.25,
+            units: ['g/mL', 'g/L', 'kg/L']
+          },
+          n: {
+            value: 19.213,
+            units: ['mol', 'mmol']
+          },
+          comment: {
+            value: ''
+          }
+        },
+        {
+          formula: {
+            value: 'HCl'
+          },
+          mw: {
+            value: 36.461
+          },
+          equiv: {
+            value: 1
+          },
+          mass: {
+            value: 3.249,
+            units: ['mg', 'g', 'kg']
+          },
+          volume: {
+            value: 4
+          },
+          purity: {
+            value: 100
+          },
+          quantifier: {
+            value: 0.25,
+            units: ['g/mL', 'g/L', 'kg/L']
+          },
+          n: {
+            value: 19.213,
+            units: ['mol', 'mmol']
+          },
+          comment: {
+            value: 'This is a long-long-long comment'
+          }
+        }
+      ]
+    };
+
+    $scope.agentRows = {
+      sticky: [
+        {
+          value: 'Potassium hydroxide'
+        }
+
+      ],
+      body: [
+        {
+          formula: {
+            value: 'KOH'
+          },
+          mw: {
+            value: 56.106
+          },
+          equiv: {
+            value: 1
+          },
+          mass: {
+            value: 5.00,
+            units: ['mg', 'g', 'kg']
+          },
+          volume: {
+            value: 6
+          },
+          purity: {
+            value: 100
+          },
+          quantifier: {
+            value: 0.25,
+            units: ['g/mL', 'g/L', 'kg/L']
+          },
+          n: {
+            value: 19.213,
+            units: ['mol', 'mmol']
+          },
+          comment: {
+            value: ''
+          }
+        }
+      ]
+    };
+
+    $scope.productRows = {
+        sticky: [{
+          value: 'Very long name of the product, very-very looong'
+        }],
+        body: [{
+          formula: {
+            value: 'HF'
+          },
+          massExp: {
+            value: 25,
+            units: ['mg', 'g', 'kg']
+          },
+          massAct: {
+            value: 4.6,
+            units: ['mg', 'g', 'kg']
+          },
+          purity: {
+            value: 86
+          },
+          nExp: {
+            value: 1.2,
+            units: ['mol', 'mmol']
+          },
+          nAct: {
+            value: 1.1,
+            units: ['mol', 'mmol']
+          },
+          yield: {
+            value: 12
+          }
+        }]
+      };
+
+    // $scope.bodyRows = [
+    //   {
+    //     formula: {
+    //       value: 'KOH'
+    //     },
+    //     mw: {
+    //       value: 56.106
+    //     },
+    //     equiv: {
+    //       value: 1
+    //     },
+    //     mass: {
+    //       value: 5.00,
+    //       units: ['mg', 'g', 'kg']
+    //     },
+    //     volume: {
+    //       value: 6
+    //     },
+    //     purity: {
+    //       value: 100
+    //     },
+    //     quantifier: {
+    //       value: 0.25,
+    //       units: ['g/mL', 'g/L', 'kg/L']
+    //     },
+    //     n: {
+    //       value: 19.213,
+    //       units: ['mol', 'mmol']
+    //     },
+    //     comment: {
+    //       value: ''
+    //     }
+    //   },
+    //   {
+    //     formula: {
+    //       value: 'HCl'
+    //     },
+    //     mw: {
+    //       value: 36.461
+    //     },
+    //     equiv: {
+    //       value: 1
+    //     },
+    //     mass: {
+    //       value: 3.249,
+    //       units: ['mg', 'g', 'kg']
+    //     },
+    //     volume: {
+    //       value: 4
+    //     },
+    //     purity: {
+    //       value: 100
+    //     },
+    //     quantifier: {
+    //       value: 0.25,
+    //       units: ['g/mL', 'g/L', 'kg/L']
+    //     },
+    //     n: {
+    //       value: 19.213,
+    //       units: ['mol', 'mmol']
+    //     },
+    //     comment: {
+    //       value: 'This is a long-long-long comment'
+    //     }
+    //   }
+    // ];
+
+    var reactantRowsPlaceholder = {
+      newStickyRow: {
         value: ''
-      };
-      var newBodyRow = {
+      },
+      newBodyRow: {
         formula: {
           value: ''
         },
@@ -118,10 +264,87 @@
         comment: {
           value: ''
         }
+      }
+    };
+
+    $scope.addNew = function (arr, type) {
+      var reactantRowsPlaceholder = {
+        newStickyRow: {
+          value: ''
+        },
+        newBodyRow: {
+          formula: {
+            value: ''
+          },
+          mw: {
+            value: null
+          },
+          equiv: {
+            value: null
+          },
+          mass: {
+            value: null,
+            units: ['mg', 'g', 'kg']
+          },
+          volume: {
+            value: null
+          },
+          purity: {
+            value: null
+          },
+          quantifier: {
+            value: null,
+            units: ['g/mL', 'g/L', 'kg/L']
+          },
+          n: {
+            value: null,
+            units: ['mol', 'mmol']
+          },
+          comment: {
+            value: ''
+          }
+        }
       };
 
-      $scope.stickyRows.push(newStickyRow);
-      $scope.bodyRows.push(newBodyRow);
+      var agentRowsPlaceholder = {
+        newStickyRow: {
+          value: ''
+        },
+        newBodyRow: {
+          formula: {
+            value: ''
+          },
+          massExp: {
+            value: null,
+            units: ['mg', 'g', 'kg']
+          },
+          massAct: {
+            value: null,
+            units: ['mg', 'g', 'kg']
+          },
+          purity: {
+            value: null
+          },
+          nExp: {
+            value: null,
+            units: ['mol', 'mmol']
+          },
+          nAct: {
+            value: null,
+            units: ['mol', 'mmol']
+          },
+          Yield: {
+            value: null
+          }
+        }
+      };
+      if (type === 'reactant') {
+        arr.sticky.push(reactantRowsPlaceholder.newStickyRow);
+        arr.body.push(reactantRowsPlaceholder.newBodyRow);
+      } else if (type === 'agent') {
+        arr.sticky.push(agentRowsPlaceholder.newStickyRow);
+        arr.body.push(agentRowsPlaceholder.newBodyRow);
+      }
     };
 
   }
@@ -149,7 +372,7 @@
       setBehavior: setBehavior,
       toggled: toggled,
       toggleDropdown: toggleDropdown,
-      unit: $scope.componentData.units ? $scope.componentData.units[0]: undefined,
+      unit: $scope.componentData.units ? $scope.componentData.units[0] : undefined,
       status: {
         isopen: false
       }
